0fbab0b9e0fcf16cfd5a3c4fee9bd3e8f6eb3ea8
JacbGit <127043001+JacbGit@users.noreply.github.com>
2023-04-15 21:02:30 -0600

    Add files via upload
    Camera, flip

---
+++ jimmy.h
@@ -0,0 +1,6 @@
+#include "raylib.h"
+#ifndef JIMMY
+#define JIMMY 0
+int foo = LOG_ERROR;
+#endif
+
--- main.c
+++ main.c
@@ -1,13 +1,82 @@
-#include <raylib.h>
+#include "raylib.h"
+#include "jimmy.h"
 
-int main(int argc, char const *argv[])
-{
-    InitWindow(600, 600, "Amongus Shooter");
-    SetTargetFPS(60);
-    while(!WindowShouldClose()) {
+int main() {
+    const int screenSize = 512;
+    InitWindow(screenSize, screenSize, "JimmyGame");
+    SetTargetFPS(30);
+    TraceLog(foo,"HOLA MUNDO, Esto es un error");
+
+    bool blink = true;
+    Texture mogus = LoadTexture("sus2.png");
+    Texture map = LoadTexture("map.png");
+    Vector2 moguspos = {256,256};
+    Vector2 mappos = {0,0};
+    Vector2 mousepos;
+    Rectangle playerhitbox = {16,16,16,16};
+    Camera2D camera = {moguspos, { screenSize/2.0f, screenSize/2.0f }, 0.0f, 7.0f };
+
+    while (!WindowShouldClose())
+    {
+        Vector2 mouseWorldPos = GetScreenToWorld2D(GetMousePosition(), camera);
+
+        blink = !blink;
+        int mogusDs = 2;
+        int mogusNs = 3;
+
+        if (mouseWorldPos.x<moguspos.x+8){
+            playerhitbox.width = -16;
+            }
+        if (mouseWorldPos.x>moguspos.x+8){
+            playerhitbox.width = 16;
+            }
+
+        if(moguspos.x<=map.width&&moguspos.y<=map.height){
+            if(IsKeyDown(KEY_W)&&IsKeyDown(KEY_D)){
+            moguspos.y -= mogusDs;
+            moguspos.x += mogusDs;
+            mousepos.x += mogusDs;
+            }
+            else if(IsKeyDown(KEY_S)&&IsKeyDown(KEY_D)){
+                moguspos.y += mogusDs;
+                moguspos.x += mogusDs;
+                mousepos.x += mogusDs;
+            }
+            else if(IsKeyDown(KEY_W)&&IsKeyDown(KEY_A)){
+                moguspos.y -= mogusDs;
+                moguspos.x -= mogusDs;
+                mousepos.x -= mogusDs;
+            }
+            else if(IsKeyDown(KEY_S)&&IsKeyDown(KEY_A)){
+                moguspos.y += mogusDs;
+                moguspos.x -= mogusDs;
+                mousepos.x -= mogusDs;
+            }
+            else if(IsKeyDown(KEY_A)){
+                moguspos.x -= mogusNs;
+                mousepos.x -= mogusDs;
+            }
+            else if(IsKeyDown(KEY_W)){
+                moguspos.y -= mogusNs;
+            }
+            else if(IsKeyDown(KEY_S)){
+                moguspos.y += mogusNs;
+            }
+            else if(IsKeyDown(KEY_D)){
+                moguspos.x += mogusNs;
+                mousepos.x += mogusDs;
+            }
+        }
+
         BeginDrawing();
-        ClearBackground(WHITE);
+            ClearBackground(WHITE);
+        camera.target = (Vector2){ moguspos.x + 16 / 2, moguspos.y + playerhitbox.height / 2 };
+        BeginMode2D(camera);
+        DrawRectangle(16, 16, 16, 16, blink ? RED:BLUE);
+        DrawFPS(10,10);
+        DrawTextureRec(mogus,playerhitbox,moguspos,WHITE);
+        DrawTextureV(map,mappos,WHITE);
+        EndMode2D();
         EndDrawing();
     }
-    return 0;
 }
